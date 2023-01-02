032ba39d449c811eb4659ed70a609ff219980cdc
François Pottier <francois.pottier@inria.fr>
2023-01-02 15:39:13 +0100

    ThunksSimple.v: comments.

--- theories/ThunksSimple.v
+++ theories/ThunksSimple.v
@@ -7,6 +7,10 @@ From iris_time Require Import TimeCredits Auth_max_nat.
    ESOP 2019 paper. It does not enjoy the consequence rule. A more elaborate
    formalization (with the same HeapLang code) can be found in Thunks.v. *)
 
+(* -------------------------------------------------------------------------- *)
+
+(* Notations that make the HeapLang code more readable. *)
+
 Notation UNEVALUATED f := (InjL f%V) (only parsing).
 Notation EVALUATED v := (InjR v%V) (only parsing).
 Notation UNEVALUATEDV f := (InjLV f%V) (only parsing).
@@ -14,6 +18,19 @@ Notation EVALUATEDV v := (InjRV v%V) (only parsing).
 Notation "'match:' e0 'with' 'UNEVALUATED' x1 => e1 | 'EVALUATED' x2 => e2 'end'" :=
   (Match e0 x1%bind e1 x2%bind e2)
   (e0, e1, x2, e2 at level 200, only parsing) : expr_scope.
+
+(* -------------------------------------------------------------------------- *)
+
+(* The HeapLang code. *)
+
+(* There are only two operations on thunks: [create] and [force]. *)
+
+(* A thunk is either white (unevaluated) or black (evaluated). There is no
+   grey color (which would mean that a thunk is currently being evaluated) and
+   there is no runtime check against reentrancy. Yet, reentrancy is dangerous:
+   we wish to guarantee that the function [f] is invoked at most once, but a
+   reentrant call to [force] could violate this property. Thus, reentrancy
+   must be statically ruled out by the specification of thunks. *)
 
 Definition create : val :=
   λ: "f",
@@ -29,6 +46,8 @@ Definition force : val :=
     | EVALUATED "v" =>
         "v"
     end.
+
+(* -------------------------------------------------------------------------- *)
 
 Section ThunkSimpleProofs.
 
