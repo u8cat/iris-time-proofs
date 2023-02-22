cc6e60e7dc0e3c8731ed65bbfa67931f5c0d5898
François Pottier <francois.pottier@inria.fr>
2023-02-22 21:30:51 +0100

    Include setup.sh in archive.

--- make_archive.sh
+++ make_archive.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
+
 ARCHIVE=iris-time-proofs.tar.gz
-rm -f $ARCHIVE
+rm -f ${ARCHIVE}
 find theories/ -type f -name '*.v' -exec \
-	tar -czvf $ARCHIVE README.md Makefile _CoqProject {} \+
+	tar -czvf ${ARCHIVE} Makefile README.md _CoqProject setup.sh {} \+
