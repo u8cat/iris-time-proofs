2034cd2d7fe4e06514d2a004204e33708884798a
François Pottier <francois.pottier@inria.fr>
2023-02-22 21:36:30 +0100

    Add create-archive.sh.

---
+++ create-archive.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if command -v gnutar >/dev/null ; then
+  # On MacOS, run "sudo port install gnutar" to obtain gnutar.
+  TAR=gnutar
+else
+  TAR=tar
+fi
+
+ARCHIVE=coq-iris-time
+
+rm -rf ${ARCHIVE} ${ARCHIVE}.tar.gz
+
+mkdir ${ARCHIVE}
+
+cp -r \
+  Makefile \
+  README.md \
+  theories \
+  setup.sh \
+  _CoqProject \
+  ${ARCHIVE}
+
+$TAR cvfz ${ARCHIVE}.tar.gz \
+  --exclude-ignore-recursive=.gitignore \
+  --owner=0 --group=0 \
+  ${ARCHIVE}
+
+rm -rf ${ARCHIVE}
